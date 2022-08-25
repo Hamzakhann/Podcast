a9a75f56726999eab81591f02316c99500396c05
Hamza Khan <hamzakhann66@gmail.com>
2022-08-25 06:30:03 -0700

    fix the import error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig
+// module.exports = {
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  nextConfig: {
+    reactStrictMode: true,
+  },
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
--- pages/single-blog/index.js
+++ pages/single-blog/index.js
@@ -4,8 +4,8 @@ import Business from "../../components/SinglePodcast/Business/Business";
 import MakerContent from "../../components/SinglePodcast/MakerContent/MakerContent";
 import BasicLayout from "../../layout/BasicLayout/BasicLayout";
 import Blog from "../../components/SingleBlog/Blog/Blog";
-import Receive from "../components/Receive/Receive";
-import Articles from "../components/Articles/Articles";
+import Receive from "../../components/Receive/Receive";
+import Articles from "../../components/Articles/Articles";
 
 const SingleBlog = () => {
   return (
